9841618797c26646ca1b86512b19d94f526e94ec
Raj Kanchan <iamraajkanchan@gmail.com>
2023-02-12 14:07:26 +0530

    LocationService a Binder Service is added

---
+++ app/src/main/java/com/example/geolocation_android/LocationService.kt
@@ -0,0 +1,73 @@
+package com.example.geolocation_android
+
+import android.annotation.SuppressLint
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import android.os.Looper
+import android.widget.Toast
+import com.google.android.gms.location.*
+
+class LocationService : Service() {
+
+    private var locationBinder: LocationBinder? = null
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    var latitude: String = ""
+    var longitude: String = ""
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val location = locationResult.lastLocation
+            latitude = location.latitude.toString()
+            longitude = location.longitude.toString()
+        }
+
+        override fun onLocationAvailability(locationAvailability: LocationAvailability) {
+            super.onLocationAvailability(locationAvailability)
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return locationBinder
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        locationBinder = LocationBinder()
+        fusedLocationProviderClient = FusedLocationProviderClient(applicationContext)
+        getLastLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        val locationRequest = LocationRequest()
+        locationRequest.interval = 50000
+        locationRequest.fastestInterval = 50000
+        locationRequest.smallestDisplacement = 170f // 170m = 0.1 mile
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.numUpdates = 1
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationProviderClient?.requestLocationUpdates(
+            locationRequest, locationCallback, Looper.myLooper()!!
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        fusedLocationProviderClient?.lastLocation?.addOnCompleteListener {
+            val location = it.result
+            if (location == null) {
+                requestNewLocationData()
+            } else {
+                latitude = location.latitude.toString()
+                longitude = location.longitude.toString()
+                Toast.makeText(applicationContext, "Latitude : $latitude, Longitude: $longitude", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    inner class LocationBinder : Binder() {
+        fun getService() : LocationService = this@LocationService
+    }
+}
