08ebea1f35afb41c163d64a41b2d1fa5794a789d
Raj Kanchan <iamraajkanchan@gmail.com>
2023-02-13 18:25:29 +0530

    PlainActivity is added.

---
+++ app/src/main/java/com/example/geolocation_android/PlainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.geolocation_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plain)
+    }
+}
