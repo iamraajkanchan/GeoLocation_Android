0f2d1e6116139037df246cbb63854323523d9351
Raj Kanchan <iamraajkanchan@gmail.com>
2023-02-14 18:11:31 +0530

    Note is added in ILocation.kt

--- app/src/main/java/com/example/geolocation_android/ILocation.kt
+++ app/src/main/java/com/example/geolocation_android/ILocation.kt
@@ -1,5 +1,9 @@
 package com.example.geolocation_android
 
+/**
+ * Note: This interface is not used in the project because we have used LocalBroadcastManager.
+ * */
+
 interface ILocation {
     fun getLocationLatitudeAndLongitude(latitude: String, longitude: String)
 }
